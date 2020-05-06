41c44a1dd79dc17a64e6473b140a64a234af1ea1
Deichindianer <boeschenphilipp@gmail.com>
2020-05-06 11:38:14 +0200

    Trying to fix the deployment step

--- create_deployment_package.sh
+++ create_deployment_package.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SHORT_SHA=$(git rev-parse HEAD)
 mkdir -p src/packages
-cd src/ && pip install -r poe_ladder_exporter/requirements.txt --target ./packages
+cd src/ && pip install --system -r poe_ladder_exporter/requirements.txt --target ./packages
 cd packages && zip -r9 $OLDPWD/function-$SHORT_SHA.zip .
 cd $OLDPWD && zip -ur function-$SHORT_SHA.zip poe_ladder_exporter
 rm -rf packages
