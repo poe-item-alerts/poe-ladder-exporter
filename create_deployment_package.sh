98dee9b6846d38ddd0c8ff32d2dd234ec4074c3d
Deichindianer <boeschenphilipp@gmail.com>
2020-05-08 18:47:21 +0200

    Adding modified workflow and local deployment package creation

--- create_deployment_package.sh
+++ create_deployment_package.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SHORT_SHA=$(git rev-parse HEAD)
 mkdir -p src/packages
-cd src/ && pip install --system -r poe_ladder_exporter/requirements.txt --target ./packages
-cd packages && zip -r9 $OLDPWD/function-$SHORT_SHA.zip .
-cd $OLDPWD && zip -ur function-$SHORT_SHA.zip poe_ladder_exporter
+cd src/ && pip install -r $1/requirements.txt --target ./packages
+cd packages && zip -r9 $OLDPWD/$1-$SHORT_SHA.zip .
+cd $OLDPWD && zip -ur $1-$SHORT_SHA.zip $1
 rm -rf packages
